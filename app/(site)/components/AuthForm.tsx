740126d60ebd6e24e7095b2bba508099598f347a
RadityaDito <raditya.ihsan@ui.ac.id>
2023-08-05 00:31:27 +0700

    Add auth page and form components

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
+import { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    if (variant === "REGISTER") {
+      //Axios Register
+    }
+    if (variant === "LOGIN") {
+      //NextAuth SignIn
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    //NextAuth Social SignIn
+  };
+
+  return (
+    <div className="mt-8 mx-auto w-full max-w-md">
+      <div className="bg-white shadow sm:rounded-lg px-4 py-8 sm:px-10 ">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <Button disabled={isLoading} fullWidth type="submit">
+            {variant === "LOGIN" ? "Sign in" : "Register"}
+          </Button>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or Continue With
+              </span>
+            </div>
+          </div>
+          <div className="">
+            <AuthSocialButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
---
+++ app/(site)/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import AuthForm from "./components/AuthForm";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center bg-gray-100 ">
+      <div className="w-full max-w-md mx-auto">
+        <Image
+          alt="logo"
+          height={48}
+          width={48}
+          className="mx-auto"
+          src={"/images/logo.png"}
+        />
+        <h2 className="font-bold text-3xl text-center mt-6 tracking-tight text-gray-900">
+          Sign in to yout account
+        </h2>
+      </div>
+      <AuthForm />
+    </div>
+  );
+}
