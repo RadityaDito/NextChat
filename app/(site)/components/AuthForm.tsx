569aaceda446061e06e1f0ebbd15617e1fb4e3fb
RadityaDito <raditya.ihsan@ui.ac.id>
2023-08-05 16:51:42 +0700

    Initiate prisma with mongodb and database schema

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,6 +4,7 @@ import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 import AuthSocialButton from "./AuthSocialButton";
 
 type Variant = "LOGIN" | "REGISTER";
@@ -53,7 +54,13 @@ const AuthForm = () => {
       <div className="bg-white shadow sm:rounded-lg px-4 py-8 sm:px-10 ">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           {variant === "REGISTER" && (
-            <Input id="name" label="Name" register={register} errors={errors} />
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
           )}
           <Input
             id="email"
@@ -61,6 +68,7 @@ const AuthForm = () => {
             type="email"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <Input
             id="password"
@@ -68,6 +76,7 @@ const AuthForm = () => {
             type="password"
             register={register}
             errors={errors}
+            disabled={isLoading}
           />
           <Button disabled={isLoading} fullWidth type="submit">
             {variant === "LOGIN" ? "Sign in" : "Register"}
@@ -80,13 +89,30 @@ const AuthForm = () => {
               <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="bg-white px-2 text-gray-500">
-                Or Continue With
+              <span className="bg-white px-2 text-gray-500 ">
+                Or continue with
               </span>
             </div>
           </div>
+          <div className=" flex  mt-6 gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+        <div className="mt-6 text-gray-500 text-sm px-2 flex justify-center gap-2">
           <div className="">
-            <AuthSocialButton />
+            {variant === "LOGIN"
+              ? "New to Messenger?"
+              : "Already have an account?"}
+          </div>
+          <div className="underline cursor-pointer" onClick={toggleVariant}>
+            {variant === "LOGIN" ? "Create an account" : "Login"}
           </div>
         </div>
       </div>
--- app/(site)/components/AuthSocialButton.tsx
+++ app/(site)/components/AuthSocialButton.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { IconType } from "react-icons";
 
-const AuthSocialButton = () => {
-  return <div>AuthSocialButton</div>;
+interface AuthSocialButtonProps {
+  icon: IconType;
+  onClick: () => void;
+}
+
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
+  icon: Icon,
+  onClick,
+}) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="flex-1 flex justify-center items-center  bg-white shadow-sm  rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-50 ring-1 
+      ring-inset 
+      ring-gray-300 
+      focus:outline-offset-0"
+    >
+      <Icon />
+    </button>
+  );
 };
 
 export default AuthSocialButton;
